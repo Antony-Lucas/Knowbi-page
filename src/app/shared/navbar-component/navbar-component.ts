655812823e3eacf19116e17ca9c31cbfa2daca54
Antony-Lucas <lawokahama@gmail.com>
2024-07-18 10:56:27 -0300

    update

--- src/app/shared/navbar-component/navbar-component.ts
+++ src/app/shared/navbar-component/navbar-component.ts
@@ -1,13 +1,37 @@
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../footer-component/footer-component';
+import { CardsValueComponent } from '../../page/home-screen/cards-values/cards.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar-component',
   standalone: true,
-  imports: [RouterModule, MatToolbarModule, NgOptimizedImage],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FooterComponent,
+    NgOptimizedImage,
+    MatToolbarModule,
+    CardsValueComponent,
+    MatButtonModule,
+    MatMenuModule,
+    FontAwesomeModule,
+  ],
   templateUrl: './navbar-component.html',
   styleUrl: './navbar-component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  icons = {
+    menu: faBars,
+  };
+
+  openClientArea() {
+    window.open('https://knowbi.com.br/suporte/', '_blank');
+  }
+}
