93a564e1fb0074c64e4a64b5688b1e249135e2f6
Antony-Lucas <lawokahama@gmail.com>
2024-07-18 13:50:21 -0300

    update

--- src/app/page/solutions/koios/koios.component.ts
+++ src/app/page/solutions/koios/koios.component.ts
@@ -1,7 +1,12 @@
-import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  CommonModule,
+  isPlatformBrowser,
+  NgOptimizedImage,
+} from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { FooterComponent } from '../../../shared/footer-component/footer-component';
 import { NavbarComponent } from '../../../shared/navbar-component/navbar-component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-koios',
@@ -10,4 +15,48 @@ import { NavbarComponent } from '../../../shared/navbar-component/navbar-compone
   templateUrl: './koios.component.html',
   styleUrl: './koios.component.css',
 })
-export class KoiosComponent {}
+export class KoiosComponent implements OnInit {
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {}
+
+  ngOnInit(): void {
+    if (this.isBrowser()) {
+      this.scrollToTop(300);
+    }
+  }
+
+  isBrowser(): boolean {
+    return isPlatformBrowser(this.platformId);
+  }
+
+  scrollToTop(duration: number): void {
+    if (!this.isBrowser()) {
+      return;
+    }
+
+    const start = window.scrollY;
+    const startTime =
+      'now' in window.performance ? performance.now() : new Date().getTime();
+
+    const scroll = (currentTime: number) => {
+      const timeElapsed = currentTime - startTime;
+      const progress = Math.min(timeElapsed / duration, 1);
+      const easeInOutCubic = (t: number) =>
+        t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+
+      window.scrollTo(0, start - start * easeInOutCubic(progress));
+
+      if (timeElapsed < duration) {
+        requestAnimationFrame(scroll);
+      }
+    };
+
+    requestAnimationFrame(scroll);
+  }
+
+  contact() {
+    this.router.navigateByUrl('/contact');
+  }
+}
